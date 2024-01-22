d067628e6e103ef4a8c3a389f30e5545b4a48cab
Daniel Solis <dsolis5323@gmail.com>
2024-01-21 23:07:17 -0600

    More rubocop fixes without frozenString

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -35,7 +35,7 @@ class ShortenedUrlsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_shortened_url
-    @shortened_url = ShortenedUrl.find_by_short_url(params[:short_url])
+    @shortened_url = ShortenedUrl.find_by(short_url: params[:short_url])
   end
 
   # Only allow a list of trusted parameters through.
