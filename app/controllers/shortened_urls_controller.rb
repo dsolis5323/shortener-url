3723b7663b10bddca671566f75c03f134c17203d
Daniel Solis <dsolis5323@gmail.com>
2024-01-21 20:23:11 -0600

    Add new routes, fix views, delete unnecessary controllers

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,22 +1,19 @@
 class ShortenedUrlsController < ApplicationController
-  before_action :set_shortened_url, only: %i[ show edit update destroy ]
+  before_action :set_shortened_url, only: %i[ show ]
 
   # GET /shortened_urls or /shortened_urls.json
   def index
-    @shortened_urls = ShortenedUrl.all
+    @shortened_urls = ShortenedUrl.order('redirects desc').limit(100)
   end
 
   # GET /shortened_urls/1 or /shortened_urls/1.json
   def show
+    redirect_to @shortened_url.original_url, allow_other_host: true
   end
 
   # GET /shortened_urls/new
   def new
     @shortened_url = ShortenedUrl.new
-  end
-
-  # GET /shortened_urls/1/edit
-  def edit
   end
 
   # POST /shortened_urls or /shortened_urls.json
@@ -34,37 +31,14 @@ class ShortenedUrlsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /shortened_urls/1 or /shortened_urls/1.json
-  def update
-    respond_to do |format|
-      if @shortened_url.update(shortened_url_params)
-        format.html { redirect_to shortened_url_url(@shortened_url), notice: "Shortened url was successfully updated." }
-        format.json { render :show, status: :ok, location: @shortened_url }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @shortened_url.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shortened_urls/1 or /shortened_urls/1.json
-  def destroy
-    @shortened_url.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to shortened_urls_url, notice: "Shortened url was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_shortened_url
-      @shortened_url = ShortenedUrl.find(params[:id])
+      @shortened_url = ShortenedUrl.find_by_short_url(params[:short_url])
     end
 
     # Only allow a list of trusted parameters through.
     def shortened_url_params
-      params.require(:shortened_url).permit(:original_url, :short_url, :title, :redirects)
+      params.require(:shortened_url).permit(:original_url)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :shortened_urls
+
+  root to: "shortened_urls#new"
+  get "/top-100", to: "shortened_urls#index"
+  get "/:short_url", to: "shortened_urls#show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
